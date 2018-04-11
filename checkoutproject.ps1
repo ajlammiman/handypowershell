5e5dd03d208bb723812f861008716f3deef1a497
Lammiman <alammiman@Companynet.org>
2018-04-11 12:55:58 +0100

    powershell to pull down boilerplate projects from git repo

---
+++ checkoutproject.ps1
@@ -0,0 +1,25 @@
+﻿$repoPath = https://github.com/ajlammiman/
+
+
+function SetUpNewReactProject($folderName) 
+{
+    git clone https://github.com/ajlammiman/reactprojectbase.git C:\inetpub\wwwroot\$folderName
+}
+
+
+function SetUpNewConsoleProject($folderName) 
+{
+   $repo = $repoPath + '/basicconsoleproject.git'
+   $path = C:\inetpub\wwwroot\$folderName
+
+    GitCheckoutRepo($repo, $path)
+}
+
+function GitCheckoutRepo($repo, $path)
+{
+    git clone $repo $path
+}
+
+
+SetUpNewConsoleProject consoleTest
+
