824dc4467359fd10fa78bc5bc685c2c09ac14ae8
Lammiman <alammiman@Companynet.org>
2018-04-19 12:58:22 +0100

    Added npm install to react project checkout script.

--- checkoutproject.ps1
+++ checkoutproject.ps1
@@ -1,11 +1,11 @@
 ﻿$repoPath = https://github.com/ajlammiman/
 
-
 function SetUpNewReactProject($folderName) 
 {
     git clone https://github.com/ajlammiman/reactprojectbase.git C:\inetpub\wwwroot\$folderName
+    cd C:\inetpub\wwwroot\$folderName
+    npm install
 }
-
 
 function SetUpNewConsoleProject($folderName) 
 {
