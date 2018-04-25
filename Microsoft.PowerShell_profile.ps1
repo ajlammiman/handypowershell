fb0a55010c10dd3036cf6bfdcf7a053fb49d4574
Lammiman <alammiman@Companynet.org>
2018-04-25 14:45:31 +0100

    Added a method that will create a repo in Git via api.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 $repoPath = 'https://github.com/ajlammiman/'
 $rootPath = 'C:\inetpub\wwwroot\'
+$token = ''
 Import-Module posh-git
 
 set-location C:\inetpub\wwwroot
@@ -21,14 +22,35 @@ function SetUpNewConsoleProject($folderName, $newRepoName)
     GitRecreateRepo($repo, $path, $newRepoName)
 }
 
-function GitRecreateRepo($oldRepo, $path, $newRepoName)
+function GitCreateNewRepo($repoName, $token)
+{
+    $Base64Token = [System.Convert]::ToBase64String([char[]]$token);
+
+    $Headers = @{
+        Authorization = 'Basic {0}' -f $Base64Token;
+    };   
+    
+    $Body = @{
+        name = $repoName;
+    } | ConvertTo-Json;
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user/repos -Body $Body -Method Post
+}
+
+function GitRecreateRepo($oldRepo, $newRepoName)
 {
     git clone --bar $repoPath + $oldRepo + '.git'
     cd $oldRepo + '.git'
+
+    GitCreateNewRepo($repoName, $token)
+
     git push --mirror $repoPath + $newRepoName
     cd ..
     rm -rf $oldRepo + '.git'
 }
 
 
+
+GitRecreateRepo 'basicconsoleproject' 'TestRepo'
 
